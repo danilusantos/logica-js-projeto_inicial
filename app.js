902582e65d27ca6447098e2f9e393b655ffc62df
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 20:54:43 -0300

    feat: :sparkles: 'else' and comments

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
 
 // número a ser adivinhado
-let numeroSecreto = 29;
+let numeroSecreto = 9;
+console.log(numeroSecreto);
 // entrada do usuário para testar o número
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 10');
 
 // se a entrada do usuário for igual ao número a ser adivinhado
 if (numeroSecreto == chute) {
-    console.log('Você acertou! (' + numeroSecreto + ')');
+    alert('Você acertou! (' + numeroSecreto + ')');
+} else {
+    alert('Você errou :(');
 }
