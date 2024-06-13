ea262e02939a05520c33109d72c6b3488b18197c
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 20:31:32 -0300

    feat: :sparkles: comparing variables

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
 
+// número a ser adivinhado
 let numeroSecreto = 29;
+// entrada do usuário para testar o número
+let chute = prompt('Escolha um número entre 1 e 30');
 
-prompt('Escolha um número entre 1 e 30');
+// se a entrada do usuário for igual ao número a ser adivinhado
+if (numeroSecreto == chute) {
+    console.log('Você acertou! (' + numeroSecreto + ')');
+}
