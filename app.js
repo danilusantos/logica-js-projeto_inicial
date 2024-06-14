d6ed684a18ecdc80d3d28c6417b9820a99fed384
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 22:36:04 -0300

    feat: :sparkles: using Math random

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
 
 // número a ser adivinhado
-let numeroSecreto = 9;
+let numeroSecreto = parseInt(Math.random() * 10) + 1; // Agora gerado aleatoriamente
 console.log(numeroSecreto);
 
 let chute = 1;
 let tentativas = 1;
 
 // enquanto chute for diferente de numeroSecreto
-while (chute != numeroSecreto) {
+/* while (chute != numeroSecreto) {
     // entrada do usuário para testar o número
     chute = prompt('Escolha um número entre 1 e 10');
     // se a entrada do usuário for igual ao número a ser adivinhado
@@ -23,6 +23,6 @@ while (chute != numeroSecreto) {
     }
     // tentativas = tentativas + 1;
     tentativas++;
-}
+} */
 
 alert(`Você acertou! (${numeroSecreto}), com ${tentativas} ${tentativas == 1 ? 'tentativa' : 'tentativas'}.`);
