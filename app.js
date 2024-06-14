42698a91bf3b0873b706ecfab85d6cb99d4bd795
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 21:46:36 -0300

    feat: :sparkles: implanting a loop to the game

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ alert('Boas vindas ao jogo do número secreto!');
 // número a ser adivinhado
 let numeroSecreto = 9;
 console.log(numeroSecreto);
-// entrada do usuário para testar o número
-let chute = prompt('Escolha um número entre 1 e 10');
 
-// se a entrada do usuário for igual ao número a ser adivinhado
-if (chute == numeroSecreto) {
-    alert(`Você acertou! (${numeroSecreto})`);
-} else {
-    if (chute > numeroSecreto) {
-        alert('O número secreto é menor que ' + chute);
+let chute;
+
+// enquanto chute for diferente de numeroSecreto
+while (chute != numeroSecreto) {
+
+    // entrada do usuário para testar o número
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se a entrada do usuário for igual ao número a ser adivinhado
+    if (chute != numeroSecreto) {
+        if (chute > numeroSecreto) {
+            alert('O número secreto é menor que ' + chute);
+        } else {
+            alert('O número secreto é maior que ' + chute);
+        }
     } else {
-        alert('O número secreto é maior que ' + chute);
+        alert(`Você acertou! (${numeroSecreto})`);
     }
+
 }
