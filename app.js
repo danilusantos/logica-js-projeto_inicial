e065d75bd51f69faa52c646da6f9e71c44a41bb8
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 22:41:23 -0300

    feat: :sparkles: enabling the code and increasing the difficulty

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 alert('Boas vindas ao jogo do número secreto!');
 
+// range para aumentar a dificuldade da aleatoriedade
+let numeroDificuldade = 100;
+
 // número a ser adivinhado
-let numeroSecreto = parseInt(Math.random() * 10) + 1; // Agora gerado aleatoriamente
+let numeroSecreto = parseInt(Math.random() * numeroDificuldade + 1) ; // Agora gerado aleatoriamente
 console.log(numeroSecreto);
 
 let chute = 1;
 let tentativas = 1;
 
 // enquanto chute for diferente de numeroSecreto
-/* while (chute != numeroSecreto) {
+while (chute != numeroSecreto) {
     // entrada do usuário para testar o número
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroDificuldade}`);
     // se a entrada do usuário for igual ao número a ser adivinhado
     if (chute != numeroSecreto) {
         if (chute > numeroSecreto) {
@@ -23,6 +26,6 @@ let tentativas = 1;
     }
     // tentativas = tentativas + 1;
     tentativas++;
-} */
+}
 
 alert(`Você acertou! (${numeroSecreto}), com ${tentativas} ${tentativas == 1 ? 'tentativa' : 'tentativas'}.`);
