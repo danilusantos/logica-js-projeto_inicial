f041380d47f08ee7ed197306e20cc353e7233e12
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 22:01:58 -0300

    feat: :sparkles: adding attempts to the code

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let numeroSecreto = 9;
 console.log(numeroSecreto);
 
 let chute;
+let tentativas = 1;
 
 // enquanto chute for diferente de numeroSecreto
 while (chute != numeroSecreto) {
-
     // entrada do usuário para testar o número
     chute = prompt('Escolha um número entre 1 e 10');
     // se a entrada do usuário for igual ao número a ser adivinhado
@@ -19,7 +19,8 @@ while (chute != numeroSecreto) {
             alert('O número secreto é maior que ' + chute);
         }
     } else {
-        alert(`Você acertou! (${numeroSecreto})`);
+        alert(`Você acertou! (${numeroSecreto}), com ${tentativas} tentativas.`);
     }
-
+    // tentativas = tentativas + 1;
+    tentativas++;
 }
