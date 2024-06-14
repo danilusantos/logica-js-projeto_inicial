dec3787663b81962ec0e7a8b3c502e0018500b5f
Danilo Santos <danilondosantos@gmail.com>
2024-06-13 22:11:35 -0300

    feat: :sparkles: adding desafio 3 test into project

---
+++ desafio3.js
@@ -0,0 +1,35 @@
+// 1
+let contadorMais = 1;
+
+while (contadorMais <= 10) {
+    console.log(contadorMais);
+    contadorMais++;
+}
+
+// 2
+let contadorMenos = 10;
+
+while (contadorMenos >= 0) {
+    console.log(contadorMenos);
+    contadorMenos--;
+}
+
+// 3
+
+let contagemRegressiva = prompt('Digite um número para a contagem: ');
+
+while (contagemRegressiva >= 0) {
+    console.log(contagemRegressiva);
+    contagemRegressiva--;
+}
+
+// 4
+
+let contagemProgressiva = prompt('Digite um número para a contagem: ');
+let contador = 0;
+
+while (contador <= contagemProgressiva) {
+    console.log(contador);
+    contador++;
+}
+
